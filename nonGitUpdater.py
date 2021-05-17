be5bfb83db41684e537688de4844605b898daea3
SamuelH91 <samuel.hyyppa@windowslive.com>
2021-05-17 23:54:20 +0300

    Update first itself then others by running again same file with additional argument

--- nonGitUpdater.py
+++ nonGitUpdater.py
@@ -1,11 +1,12 @@
 import wget
 import os
+import sys
 
+updater_file = "nonGitUpdater.py"
 base_url = "https://raw.githubusercontent.com/SamuelH91/Civ6EGRM/master/"
 files = [
     "endGameReplay.bat",
     "endGameReplay.py",
-    "nonGitUpdater.py",
     "folderWatcher/__init__.py",
     "folderWatcher/autoRunEndGameReplayHere.bat",
     "folderWatcher/fileCopier.py",
@@ -21,8 +22,18 @@ files = [
     "utils/hexgrid.py",
 ]
 
-print(f"This file should be run from the root of the Civ6EGRM folder!!!")
-print(f"Updating common files without git: Use git for to be sure that all necessary files are up-to-date!")
+if len(sys.argv) < 2:
+    print(f"This file should be run from the root of the Civ6EGRM folder!!!")
+    print(f"Updating common files without git: Use git for to be sure that all necessary files are up-to-date!")
+    print(f"Updating this file first")
+    if os.path.exists(updater_file):  # remove old first
+        os.remove(updater_file)
+    wget.download(base_url + updater_file, updater_file)
+    new_args = sys.argv[1:]
+    new_args.append("UpdateOtherFiles")
+    os.execl(sys.executable, 'python', __file__, *new_args)  # Restart this script with additional arg
+else:
+    print(f"\nUpdating other files next:")
 
 for file in files:
     print(f"\nDownloading file '{file}'")
